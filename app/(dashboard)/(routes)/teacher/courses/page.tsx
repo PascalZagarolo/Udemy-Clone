bbe19aa506dc19cd95ccdd5865ab0a88ada4a5e3
Pascal <zagarolopascal@gmail.com>
2023-10-15 13:18:15 +0200

    Final v2

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,42 +1,30 @@
-
-
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { DataTable } from "./_components/data-table";
-import { columns } from "./_components/columns";
-import axios from "axios";
-import toast from "react-hot-toast";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
-import Image from "next/image";
+import { columns } from "./_components/columns";
 
+const CoursesPage = async () => {
+  const { userId } = auth();
 
+  if (!userId) {
+    return redirect("/");
+  }
 
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-export const CoursesPage = async () => {
-
-    const { userId } = auth();
-
-    if(!userId) {
-        redirect("/");
-    }
-
-    const courses = await db.course.findMany({
-        where :{
-            userId,
-        }, orderBy : {
-            createdAt : "desc"
-        }
-    })
-
-    
-
-    return ( 
-        <div className="p-6">
-           <DataTable columns={columns} data={courses} />
-        </div>
-     );
+  return ( 
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
+    </div>
+   );
 }
  
 export default CoursesPage;
