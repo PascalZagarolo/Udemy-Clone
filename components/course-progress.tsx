41dc488e996833abbf0917f9a9ca17d003895c46
Pascal <zagarolopascal@gmail.com>
2023-10-31 11:36:01 +0100

    Course-Card update

--- components/course-progress.tsx
+++ components/course-progress.tsx
@@ -3,43 +3,53 @@ import { Progress } from "./ui/progress";
 import { BarChart2 } from "lucide-react";
 
 interface CourseProgressProps {
-    variant? : "success" | "default";
-    value : number;
-    size? : "default" | "success"
+    variant?: "success" | "default";
+    value: number;
+    size?: "default" | "success"
 
 }
 
 const colorByVariant = {
-    default : "text-sky-700",
-    success : "text-sky-700"
+    default: "text-sky-700",
+    success: "text-sky-700"
 }
 
 const sizeByVariant = {
-    default : "text-sm",
-    success : "text-xs"
+    default: "text-sm",
+    success: "text-xs"
 }
 
 const CourseProgress: React.FC<CourseProgressProps> = ({
     variant,
     value
 }) => {
-    return ( 
+    return (
         <div>
-            <Progress
-            className="h-2"
-            value={value}
-            variant={variant}
+            {value > 100 ? (
+                <div>
+                <p className={cn("font-medium mt-2 text-sky-700",
+                    colorByVariant[variant || "default"],
+                    sizeByVariant[variant || "default"],)}>
+
+                    Kurs zu {value} % abgeschlossen.
+
+                </p>
+                <Progress
+                className="h-2"
+                value={value}
+                variant={variant}
             />
+            </div>
+            ): (
+                <p className="text-sm text-blue-800 font-semibold">
+                    Kurs erfolgreich abgeschlossen!
+                </p>
+            )}
             
-            <p className={cn("font-medium mt-2 text-sky-700",
-                colorByVariant[variant || "default"],
-                sizeByVariant[variant || "default"], )}>
-                    
-                Kurs zu {value} % abgeschlossen.
-                
-            </p>
+
+
         </div>
-     );
+    );
 }
- 
+
 export default CourseProgress;
