ef67b8b86d1412bdef5d7195152c6aae2d44d395
Pascal <zagarolopascal@gmail.com>
2023-11-26 22:50:09 +0100

    ...

--- app/(course)/courses/[courseId]/chapter/[chapterId]/_components/_rating-components/rating-course-dialog.tsx
+++ app/(course)/courses/[courseId]/chapter/[chapterId]/_components/_rating-components/rating-course-dialog.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 
-import { Star } from "lucide-react";
+import { Star, StarHalf } from "lucide-react";
 
 const RatingDialog = () => {
 
     const starWrapper = document.querySelector(".stars");
     const stars = document.querySelector(".stars a");
 
-    const onClick = (rating : number) => {
+    const onClick = (rating: number) => {
 
         console.log(rating);
     }
@@ -25,7 +25,8 @@ const RatingDialog = () => {
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>
+                    <DialogTitle className="flex justify-start">
+                        <StarHalf className="text-yellow-400" />
                         Gebe diesem Kurs eine Bewertung!
                     </DialogTitle>
                     <p className="text-gray-800/80 text-sm"> Bewertungen sind für den Kurs-Ersteller sichtbar.</p>
@@ -33,17 +34,27 @@ const RatingDialog = () => {
                 <div>
                     <div>
                         <div className="stars text-2xl">
-                            <a type="star"><button type="button" onClick={() => {onClick(1)}}>⭐</button></a>
-                            <a type="star"><button type="button" onClick={() => {onClick(2)}}>⭐</button></a>
-                            <a type="star"><button type="button" onClick={() => {onClick(3)}}>⭐</button></a>
-                            <a type="star"><button type="button" onClick={() => {onClick(4)}}>⭐</button></a>
-                            <a type="star"><button type="button" onClick={() => {onClick(5)}}>⭐</button></a>
+                            <a type="star"><button type="button" onClick={() => { onClick(1) }}>⭐</button></a>
+                            <a type="star"><button type="button" onClick={() => { onClick(2) }}>⭐</button></a>
+                            <a type="star"><button type="button" onClick={() => { onClick(3) }}>⭐</button></a>
+                            <a type="star"><button type="button" onClick={() => { onClick(4) }}>⭐</button></a>
+                            <a type="star"><button type="button" onClick={() => { onClick(5) }}>⭐</button></a>
                         </div>
+                        <p className="text-gray-800/50 text-xs"> Noch keine Bewertung abgegeben.</p>
                     </div>
                 </div>
+                <DialogFooter>
+                    <DialogTrigger>
+                        <Button className="bg-blue-800">
+                            Bewertung abschicken
+                        </Button>
+                    </DialogTrigger>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     );
 }
 
 export default RatingDialog;
+
+
