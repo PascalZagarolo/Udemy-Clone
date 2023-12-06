6dc661248164cc8af9b16baae58016b65f09a7a8
Pascal <zagarolopascal@gmail.com>
2023-12-06 23:52:40 +0100

    ...

--- app/(profilepage)/user/[userId]/chat/_components/chat-input.tsx
+++ app/(profilepage)/user/[userId]/chat/_components/chat-input.tsx
@@ -4,14 +4,18 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import { Send } from "lucide-react";
+import { useParams } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 const ChatInput = () => {
 
     const formSchema = z.object({
-        message: z.string().min(1, {
+        content: z.string().min(1, {
             message: "Nachricht ist zu kurz"
         })
     })
@@ -19,9 +23,24 @@ const ChatInput = () => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            message: ""
+            content: ""
         }
     })
+
+    const [isLoading, setIsLoading] = useState(false);
+    const params = useParams();
+
+    const onSubmit = (values : z.infer<typeof formSchema>) => {
+        try {
+            setIsLoading(true);
+            axios.post(`/api/user/${params.userId}/message` , values);
+            toast.success("Nachricht erfolgreich gesendet");
+        } catch {
+            toast.error("Nachricht konnte nicht gesendet werden");
+        } finally {
+            setIsLoading(false)
+        }
+     }
 
     const { isSubmitting, isValid } = form.formState
 
@@ -32,22 +51,23 @@ const ChatInput = () => {
                 
 
 
-                    <Form {...form}>
-                        <form onSubmit={() => { }}>
-                            <Button variant="ghost" className="ml-4" type="submit" disabled={!isValid || isSubmitting}>
+                    <Form {...form}> 
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="flex justify-start">
+                            <Button variant="ghost" className="ml-4" type="submit" disabled={!isValid} onSubmit={form.handleSubmit(onSubmit)}>
                                 <Send
                                     className="text-blue-800"
                                 />
                             </Button>
                             <FormField
                                 control={form.control}
-                                name="message"
+                                name="content"
                                 render={({ field }) => (
                                     <FormItem>
                                         <FormControl>
                                             <div className="ml-4">
                                             <Input
                                                 placeholder="Verfasse eine Nachricht"
+                                                {...field}
                                             />
                                             </div>
                                             
