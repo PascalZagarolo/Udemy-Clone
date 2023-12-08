e957c9369d40b68ea44a7ec63e7e236b4b8cda52
Pascal <zagarolopascal@gmail.com>
2023-12-08 23:51:00 +0100

    ...

--- app/(profilepage)/user/[userId]/chat/_components/chat-messages.tsx
+++ app/(profilepage)/user/[userId]/chat/_components/chat-messages.tsx
@@ -16,11 +16,11 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({
 
     return ( 
         <div className=" w-full flex ">
-            <div className={cn("justify-start items-center rounded-md", ownMessage ? "bg-blue-500/50" : "bg-emerald-500/50" )}>
-            <div className="text-base font-semibold mb-2 ml-4 mr-4 mt-2">
+            <div className={cn("justify-start items-center rounded-md", ownMessage ? "bg-blue-500/50" : "bg-green-600/20" )}>
+            <div className="text-base font-semibold mb-2 ml-4 mr-4 mt-4">
                 {content}
                 <div>
-                <p className="text-xs text-gray-500/50"> 12:13 Uhr</p>
+                <p className="text-xs text-gray-500/50 mt-2"> 12:13 Uhr</p>
                 </div>
             </div>
                 
