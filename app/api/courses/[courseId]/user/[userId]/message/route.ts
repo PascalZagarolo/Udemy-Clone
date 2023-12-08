d76dc751ac605e6a4256a441322a8d2c2618c30e
Pascal <zagarolopascal@gmail.com>
2023-12-08 14:40:40 +0100

    ...

--- app/api/courses/[courseId]/user/[userId]/message/route.ts
+++ app/api/courses/[courseId]/user/[userId]/message/route.ts
@@ -1,9 +1,54 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server";
+
 export async function POST(
-    req : Request,
-    { params } : { params : { userId : string }}
+    req: Request,
+    { params }: { params: { userId: string } }
 ) {
     try {
+        const { userId } = auth();
 
+        if (!userId) {
+            return new NextResponse("Nicht autorisiert", { status: 401 })
+        }
+
+        const values = await req.json();
+
+        const existingConversation = await db.conversation.findMany({
+            where: {
+                OR: [
+                    {
+                        user1Id: userId,
+                        user2Id: params.userId
+                    }, {
+                        user1Id: params.userId,
+                        user2Id: userId
+                    }
+                ]
+
+
+            },
+        })
+
+        if(!existingConversation) {
+            const existingConversation = await db.conversation.create({
+                data : {
+                    user1Id : params.userId,
+                    user2Id : userId
+                }
+            })
+        }
+
+        const createdMessage = await db.message.create({
+            data : {
+                conversationId : existingConversation[0].id,
+                userId : userId,
+                content : values.content
+            }
+        })
+
+        return NextResponse.json(createdMessage);
     } catch (error) {
         console.log(error)
     }
